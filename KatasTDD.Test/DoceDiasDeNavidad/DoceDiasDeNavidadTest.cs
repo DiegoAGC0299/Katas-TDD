f4bae6abea650821f295c07b1833cbfa47454cad
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-10-20 11:28:34 -0500

    🟥 Si_DiaEsUno_Debe_RetornarLetraCancion_Con_LineaCorrespondienteYRegalos

---
+++ KatasTDD.Test/DoceDiasDeNavidad/DoceDiasDeNavidadTest.cs
@@ -0,0 +1,29 @@
+﻿using AwesomeAssertions;
+
+namespace KatasTDD.Test.DoceDiasDeNavidad;
+
+public class DoceDiasDeNavidadTest
+{
+    [Fact]
+    public void Si_DiaEsUno_Debe_RetornarLetraCancion_Con_LineaCorrespondienteYRegalos()
+    {
+        var cancion = new Cancion();
+        
+        cancion.ConstruirCancion(1);
+
+        cancion.ImprimirLetra().Should().BeEquivalentTo("El primer día de navidad \n Mi verdadero amor me regaló \n Una perdiz en un arbol de peras");
+    }
+}
+
+public class Cancion
+{
+    public void ConstruirCancion(int i)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ImprimirLetra()
+    {
+        throw new NotImplementedException();
+    }
+}
