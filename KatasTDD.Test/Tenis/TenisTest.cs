44c07ab92c299847749a25fe9259a5f53be33adf
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-10-20 17:32:29 -0500

    🟥 Si_IniciaJuegoTenis_Debe_ElResultadoSerCero

---
+++ KatasTDD.Test/Tenis/TenisTest.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeAssertions;
+
+namespace KatasTDD.Test.Tenis;
+
+public class TenisTest
+{
+    [Fact]
+    public void Si_IniciaJuegoTenis_Debe_ElResultadoSerCero()
+    {
+        var juegoTennis = new JuegoTenis();
+
+        juegoTennis.ObtenerPuntuacion().Should().BeEquivalentTo("0-0");
+    }
+}
+
+public class JuegoTenis
+{
+    public object ObtenerPuntuacion()
+    {
+        throw new NotImplementedException();
+    }
+}
