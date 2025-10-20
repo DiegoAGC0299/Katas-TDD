4d2799a9ea5dbc34abd49efbe085bf10c4c434b6
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-10-20 17:49:39 -0500

    🟥 Si_UnJugadorObtieneUnPunto_Y_SuOponenteObtieneCeroPuntos_Debe_ElPuntajeDelJugadorSerQuinceYSuOponenteLove

--- KatasTDD.Test/Tenis/TenisTest.cs
+++ KatasTDD.Test/Tenis/TenisTest.cs
@@ -11,13 +11,38 @@ public class TenisTest
 
         juegoTennis.ObtenerPuntuacion().Should().BeEquivalentTo("Love-Love");
     }
+
+    [Theory]
+    [InlineData(1,0,"Fifteen-Love")]
+    [InlineData(0,1,"Love-Fifteen")]
+    public void Si_UnJugadorObtieneUnPunto_Y_SuOponenteObtieneCeroPuntos_Debe_ElPuntajeDelJugadorSerQuinceYSuOponenteLove(int puntosJugadorA, int puntosJugadorB, string puntuacionEsperada)
+    {
+        var juegoTennis = new JuegoTenis();
+        
+        juegoTennis.AgregarPuntuacionJugadorA(puntosJugadorA);
+        juegoTennis.AgregarPuntuacionJugadorB(puntosJugadorB);
+        
+        juegoTennis.ObtenerPuntuacion().Should().BeEquivalentTo(puntuacionEsperada);
+    }
 }
 
 public class JuegoTenis
 {
     private string _puntuacion = "Love-Love";
+    private int _puntajeJugadorA = 0;
+    private int _puntajeJugadorB = 0;
     public string ObtenerPuntuacion()
     {
         return _puntuacion;
+    }
+
+    public void AgregarPuntuacionJugadorA(int puntosJugadorA)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AgregarPuntuacionJugadorB(int puntosJugadorB)
+    {
+        throw new NotImplementedException();
     }
 }
