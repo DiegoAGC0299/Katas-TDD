02d4bb7466ed4afd4e424c36df15165d95ff0e31
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-02 22:58:20 -0500

    🟥🟩🟦 Si_HoraEstaEntre0600y0700_Debe_MostrarHacerEjercicio

---
+++ KatasTDD.Test/RutinaMananera/RutinaMananeraTests.cs
@@ -0,0 +1,24 @@
+﻿using AwesomeAssertions;
+
+namespace KatasTDD.Test.RutinaMananera;
+
+public class RutinaMananeraTests
+{
+    [Fact]
+    public void Si_HoraEstaEntre0600y0700_Debe_MostrarHacerEjercicio()
+    {
+        var rutinaMananera = new RutinaMananera();
+
+        var actividadParaRealizar = rutinaMananera.SolicitarActividadParaRealizar("06:00");
+
+        actividadParaRealizar.Should().BeEquivalentTo("Hacer ejercicio");
+    }
+}
+
+public class RutinaMananera
+{
+    public object SolicitarActividadParaRealizar(string hora)
+    {
+        return "Hacer ejercicio";
+    }
+}
