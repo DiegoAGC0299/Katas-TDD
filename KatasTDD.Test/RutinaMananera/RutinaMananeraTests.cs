d522739f6fdc278c3e6d544536a99f38bc5d8322
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-02 23:33:39 -0500

    🟥🟩🟦Si_HoraEstaEntre0800y0859_Debe_MostrarDesayunar

--- KatasTDD.Test/RutinaMananera/RutinaMananeraTests.cs
+++ KatasTDD.Test/RutinaMananera/RutinaMananeraTests.cs
@@ -23,16 +23,38 @@ public class RutinaMananeraTests
 
         actividadParaRealizar.Should().BeEquivalentTo("Leer y estudiar");
     }
+    
+    [Fact]
+    public void Si_HoraEstaEntre0800y0859_Debe_MostrarDesayunar()
+    {
+        var rutinaMananera = new RutinaMananera();
+
+        var actividadParaRealizar = rutinaMananera.SolicitarActividadParaRealizar("08:00");
+
+        actividadParaRealizar.Should().BeEquivalentTo("Desayunar");
+    }
 }
 
 public class RutinaMananera
 {
     public string SolicitarActividadParaRealizar(string hora)
     {
+        var actividad = string.Empty;
         var horaCalculada = TimeSpan.Parse(hora);
 
         var rangoEntre0600Y0659 = horaCalculada >= new TimeSpan(6, 0, 0) && horaCalculada < new TimeSpan(7, 0, 0);
+        var rangoEntre0700Y0759 = horaCalculada >= new TimeSpan(7, 0, 0) && horaCalculada < new TimeSpan(8, 0, 0);   
+        var rangoEntre0800Y0859 = horaCalculada >= new TimeSpan(8, 0, 0) && horaCalculada < new TimeSpan(9, 0, 0);   
         
-        return rangoEntre0600Y0659 ? "Hacer ejercicio" : "Leer y estudiar";
+        if (rangoEntre0600Y0659)
+            actividad = "Hacer ejercicio";
+        
+        if (rangoEntre0700Y0759)
+            actividad = "Leer y estudiar";
+        
+        if (rangoEntre0800Y0859)
+            actividad = "Desayunar";
+        
+        return actividad;
     }
 }
