2e25f915777e3cf839b080a0770c69506c893831
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-02 23:26:29 -0500

    🟥🟩🟦 Si_HoraEstaEntre0700y0759_Debe_MostrarLeerYEstudiar

--- KatasTDD.Test/RutinaMananera/RutinaMananeraTests.cs
+++ KatasTDD.Test/RutinaMananera/RutinaMananeraTests.cs
@@ -5,7 +5,7 @@ namespace KatasTDD.Test.RutinaMananera;
 public class RutinaMananeraTests
 {
     [Fact]
-    public void Si_HoraEstaEntre0600y0700_Debe_MostrarHacerEjercicio()
+    public void Si_HoraEstaEntre0600y0659_Debe_MostrarHacerEjercicio()
     {
         var rutinaMananera = new RutinaMananera();
 
@@ -13,12 +13,26 @@ public class RutinaMananeraTests
 
         actividadParaRealizar.Should().BeEquivalentTo("Hacer ejercicio");
     }
+
+    [Fact]
+    public void Si_HoraEstaEntre0700y0759_Debe_MostrarLeerYEstudiar()
+    {
+        var rutinaMananera = new RutinaMananera();
+
+        var actividadParaRealizar = rutinaMananera.SolicitarActividadParaRealizar("07:00");
+
+        actividadParaRealizar.Should().BeEquivalentTo("Leer y estudiar");
+    }
 }
 
 public class RutinaMananera
 {
-    public object SolicitarActividadParaRealizar(string hora)
+    public string SolicitarActividadParaRealizar(string hora)
     {
-        return "Hacer ejercicio";
+        var horaCalculada = TimeSpan.Parse(hora);
+
+        var rangoEntre0600Y0659 = horaCalculada >= new TimeSpan(6, 0, 0) && horaCalculada < new TimeSpan(7, 0, 0);
+        
+        return rangoEntre0600Y0659 ? "Hacer ejercicio" : "Leer y estudiar";
     }
 }
--- KatasTDD.Test/RutinaMananera/TestList.md
+++ KatasTDD.Test/RutinaMananera/TestList.md
@@ -1,7 +1,7 @@
 ﻿# Iteración 1
 
 - [x] Si la hora está entre las 06:00 y las 06:59, debe mostrar Hacer ejercicio
-- [] Si la hora está entre las 07:00 y las 07.59, debe mostrar Leer y estudiar
+- [x] Si la hora está entre las 07:00 y las 07.59, debe mostrar Leer y estudiar
 - [] Si la hora está entre las 08:00 y las 08:59, debe mostrar Desayunar
 - [] Si la hora está fuera del rango (menor a las 05:00 y mayor a las 09:00), debe mostrar sin actividad
 
