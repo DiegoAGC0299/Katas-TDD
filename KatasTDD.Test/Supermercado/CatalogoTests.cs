59d0dc73ea07f5a5b8c76f047b75456dfe5566f3
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-10 12:05:47 -0500

    🟥🟩🟦 Si_AgregoDosProductosAlCatalogoConElMismoNombre_Debe_LanzarExcepcion

--- KatasTDD.Test/Supermercado/CatalogoTests.cs
+++ KatasTDD.Test/Supermercado/CatalogoTests.cs
@@ -1,4 +1,5 @@
-﻿using AwesomeAssertions;
+﻿using System.Data;
+using AwesomeAssertions;
 
 namespace KatasTDD.Test.Supermercado;
 
@@ -15,6 +16,17 @@ public class CatalogoTests
         catalogo.Productos[0].Should().NotBeNull();
         catalogo.Productos[0].Should().BeEquivalentTo(producto);
     }
+
+    [Fact]
+    public void Si_AgregoDosProductosAlCatalogoConElMismoNombre_Debe_LanzarExcepcion()
+    {
+        var catalogo = new Catalogo();
+        
+        catalogo.AgregarProducto(new Producto("Manzana",0.99));
+        var caller = () => catalogo.AgregarProducto(new Producto("Manzana",1.99));
+        
+        caller.Should().ThrowExactly<DuplicateNameException>().WithMessage("Ya existe un producto con el mismo nombre.");
+    }
 }
 
 public class Catalogo
@@ -22,7 +34,14 @@ public class Catalogo
     public List<Producto> Productos { get; } = [];
     public void AgregarProducto(Producto producto)
     {
+        LanzarExcepcionSiProductoYaExisteConElMismoNombre(producto);
         Productos.Add(producto);
+    }
+
+    private void LanzarExcepcionSiProductoYaExisteConElMismoNombre(Producto producto)
+    {
+        if(Productos.Any(a => a.Nombre.ToLower().Equals(producto.Nombre.ToLower())))
+            throw new DuplicateNameException("Ya existe un producto con el mismo nombre.");
     }
 }
 
