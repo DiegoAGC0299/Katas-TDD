8701bf2d67ed70b8b06cd70d7bcb719a63a0b7c5
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-10 13:11:38 -0500

    🟥🟩🟦Si_AgregoUnProducto_Debe_AgregarloEnLaListaDeCompra

---
+++ KatasTDD.Test/Supermercado/CarritoComprasTests.cs
@@ -0,0 +1,30 @@
+﻿using AwesomeAssertions;
+
+namespace KatasTDD.Test.Supermercado;
+
+public class CarritoComprasTests
+{
+    [Fact]
+    public void Si_AgregoUnProducto_Debe_AgregarloEnLaListaDeCompra()
+    {
+        var producto = new Producto("Manzana", 1.00);
+        var catalogo = new Catalogo();
+        catalogo.AgregarProducto(producto);
+        var carritoCompras = new CarritoCompras(catalogo);
+
+        carritoCompras.AgregarProducto(producto.Nombre);
+        
+        carritoCompras.ObtenerLista()[0].Producto.Should().Be(producto);
+    }
+}
+
+public class CarritoCompras(Catalogo catalogo)
+{
+    private readonly List<ListaCompra> _listaCompra = [];
+    public void AgregarProducto(string nombreProducto) => _listaCompra.Add(new ListaCompra(ConsultarProducto(nombreProducto), 1));
+    public List<ListaCompra> ObtenerLista() => _listaCompra;
+
+    public record ListaCompra(Producto? Producto, double Cantidad);
+    
+    private Producto? ConsultarProducto(string nombreProducto) => catalogo.Productos.FirstOrDefault(f => f.Nombre == nombreProducto);
+}
