38c3adc19e04b30ca76468e4cd15236a5ce190ad
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-17 15:30:17 -0500

    🟥🟩🟦 If_WordIsEmpty_Must_ReturnEmptyString

---
+++ KatasTDD.Test/WordWrap/WordWrapTests.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeAssertions;
+
+namespace KatasTDD.Test.WordWrap;
+
+public class WordWrapTests
+{
+    [Fact]
+    public void If_WordIsEmpty_Must_ReturnEmptyString()
+    {
+        var result = WordsWrap.Wrap("", 1);
+
+        result.Should().Be("");
+    }
+}
+
+public static class WordsWrap
+{
+    public static object Wrap(string word, int col)
+    {
+        return "";
+    }
+}
