9604353a1bdbede015ac5a88bc241420751e7fdd
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-17 15:37:29 -0500

    🟥🟩🟦 If_WordIsShorterThanCol_Must_ReturnSameWord

--- KatasTDD.Test/WordWrap/WordWrapTests.cs
+++ KatasTDD.Test/WordWrap/WordWrapTests.cs
@@ -11,12 +11,20 @@ public class WordWrapTests
 
         result.Should().Be("");
     }
+
+    [Fact]
+    public void If_WordIsShorterThanCol_Must_ReturnSameWord()
+    {
+        var result = WordsWrap.Wrap("this", 10);
+
+        result.Should().Be("this");
+    }
 }
 
 public static class WordsWrap
 {
     public static object Wrap(string word, int col)
     {
-        return "";
+        return word.Length < col ? word : "";
     }
 }
